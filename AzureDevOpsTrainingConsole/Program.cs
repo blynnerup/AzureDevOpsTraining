c7e3daa7c8076bb3acd768e45e4bfdc6440c59ed
blynnerup <brian.lynnerup.pedersen@gmail.com>
2020-01-23 11:19:22 +0100

    Test pipeline

--- AzureDevOpsTrainingConsole/Program.cs
+++ AzureDevOpsTrainingConsole/Program.cs
@@ -6,7 +6,7 @@ namespace AzureDevOpsTrainingConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Azure!");
             Console.ReadKey();
         }
     }
